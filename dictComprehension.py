dc179571ab9a3c3027cd1da6df8314aa4d67061b
Pei Wang <wangpei0068@gmail.com>
2013-10-28 00:12:16 -0700

    apply filter on dictionary comprehension

--- dictComprehension.py
+++ dictComprehension.py
@@ -20,3 +20,22 @@ for i in upperLetters.keys():
 
 # need to place : after for loop
 # need identation in for loop
+
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+powof2 = {i: pow(2, i) for i in digits if i%2 == 0}
+
+print powof2
+
+# why it has syntax error ?
+# print "keys are: " + ", ".join([str(i) for i in powof2.keys()])
+# print "values are: " + ", ".join(([str(i) for i in powof2.values()])
+print "length is: " + str(len(powof2))
+
+for i in digits:
+	if i%2 == 0:
+		print str(i) + "->" + str(powof2[i])
+
+for i in powof2.keys():
+	print str(i) + "->" + str(powof2[i])
